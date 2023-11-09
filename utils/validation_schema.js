a58e1df07993f158d85df2a723537bf72b670ddc
JosephIbe <joseph.ibeawuchi@gmail.com>
2023-11-09 06:19:45 +0100

    Change Validation Schema for Passwords to Allow Special Characters

--- utils/validation_schema.js
+++ utils/validation_schema.js
@@ -4,12 +4,12 @@ const registerSchema = Joi.object({
     firstName: Joi.string().lowercase().min(2).max(10).required(),
     lastName: Joi.string().lowercase().min(2).max(15).required(),
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,15}$')).required(),
+    password: Joi.string().min(6).max(15),
 });
 
 const loginSchema = Joi.object({
     email: Joi.string().email().lowercase().trim().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,15}$')).required()
+    password: Joi.string().min(6).max(15)
 });
 
 module.exports = {
