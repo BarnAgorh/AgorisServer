849caf7f862100d2dabb73873675c2a2ac119145
JosephIbe <joseph.ibeawuchi@gmail.com>
2023-12-17 13:29:47 +0100

    Add Separate Routes for Loggedin User Profile and Other User's Profile

--- routes/profile.js
+++ routes/profile.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const {protect} = require('../middlewares/auth')
 
 const {
+    getMyProfile,
     getUserProfile,
     updateMyProfile,
     setFCMPushTokenOnUser
 } = require('../controllers/profile')
 
+router.post('/my-profile', protect, getMyProfile)
 router.post('/profile', protect, getUserProfile)
 router.put('/update-profile', protect, updateMyProfile)
 router.post('/set-fcm-token', protect, setFCMPushTokenOnUser)
